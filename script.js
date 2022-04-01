c9530623338c455b64759f03a35a93d703af05a1
Lucas Cuerci <86057967+MohamedCuerci@users.noreply.github.com>
2022-04-01 09:48:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function displayAbout() {
+    var dale = document.getElementById('animation');
+    dale.classList.toggle('flip-horizontal-bottom');
+
+    var display = document.getElementById('about');
+    display.classList.toggle('active');
+}
